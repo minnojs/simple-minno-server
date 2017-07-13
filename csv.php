8ce42d1496c2a74732fff27bd4f5f55c7b78d754
ronenno1 <ronen.no1@gmail.com>
2017-07-13 13:21:43 +0300

    ignore session

--- csv.php
+++ csv.php
@@ -1,9 +1,4 @@
 <?php   
-    if(!session_id())
-    {
-        session_start();
-        if(!isset($_SESSION['id']))
-            $_SESSION['id'] = sha1(microtime()+$_SERVER['REMOTE_ADDR']);
-    }
-    $entityBody = preg_replace('/^/m', "$_SESSION[id], ", file_get_contents('php://input'))."\n";
+    $_session_id = sha1(microtime()+$_SERVER['REMOTE_ADDR']);
+    $entityBody = preg_replace('/^/m', "$_SESSION[id], ", file_get_contents('inp.txt'))."\n";
     file_put_contents("res/$_SESSION[id].csv", $entityBody, FILE_APPEND);

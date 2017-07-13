ae78f26afa972042de0b234eb150e5ab421b3c7e
ronenno1 <ronen.no1@gmail.com>
2017-07-13 13:12:26 +0300

    create a new file for each post request

--- csv.php
+++ csv.php
@@ -6,4 +6,4 @@
             $_SESSION['id'] = sha1(microtime()+$_SERVER['REMOTE_ADDR']);
     }
     $entityBody = preg_replace('/^/m', "$_SESSION[id], ", file_get_contents('php://input'))."\n";
-    file_put_contents("$_SESSION[id].res.csv", $entityBody, FILE_APPEND);
+    file_put_contents("res/$_SESSION[id].res.csv", $entityBody, FILE_APPEND);

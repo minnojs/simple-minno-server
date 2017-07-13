d377e01e98bbf45f819588d9829924ee8ce29540
ronenno1 <ronen.no1@gmail.com>
2017-07-13 13:03:10 +0300

    add comma

--- csv.php
+++ csv.php
@@ -5,5 +5,5 @@
         if(!isset($_SESSION['id']))
             $_SESSION['id'] = sha1(microtime()+$_SERVER['REMOTE_ADDR']);
     }
-    $entityBody = preg_replace('/^/m', $_SESSION['id'], file_get_contents('php://input'))."\n";
+    $entityBody = preg_replace('/^/m', "$_SESSION[id], ", file_get_contents('php://input'))."\n";
     file_put_contents('res.csv', $entityBody, FILE_APPEND);
